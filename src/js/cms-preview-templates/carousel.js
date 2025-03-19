19bb0e5c8b56f012470dfcc0003f65812aaa77f7
Jan Thomsen <jan-thomsen@siemens.com>
2025-03-19 14:35:33 +0100

    update carousel

--- src/js/cms-preview-templates/carousel.js
+++ src/js/cms-preview-templates/carousel.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react'
 
-const CarouselEntry = ({imgPath}) =>
+const Image = ({imgPath}) =>
     <div>
       <img src={imgPath} alt="" className="db w4 center pv4" />
     </div>;
 
-const CarouselEntries = ({data}) =>
-    (data && data.length > 0
-      ? <div className="flex-ns mb3">
-        {// eslint-disable-next-line react/jsx-key
-          data.map(({imgPath}) => <CarouselEntry imgPath={imgPath} />)}
-      </div>
-      : "");
-
 export class carousel extends Component {
   render() {
-    const {entry, getAsset, widgetFor} = this.props;
-    const entryCarouselEntries = entry.getIn(["data", "carousel_entries"]);
-    const carouselEntries = entryCarouselEntries ? entryCarouselEntries.toJS() : [];
+    const { image } = this.props;
     return <div className="ph3 bg-off-white">
       <div className="center mw6 pv3">
-        { widgetFor("body") }
-        <CarouselEntries data={contactEntries} />
+        <Image imgPath={image} />
       </div>
     </div>;
   }
